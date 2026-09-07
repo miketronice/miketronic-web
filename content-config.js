f73ecbaf29242c4021ced302fe54dfaae935499a
miketronice <miketronic.es@gmail.com>
2026-09-07 21:39:43 +0200

    Add editable content cards configuration

---
+++ content-config.js
@@ -0,0 +1,30 @@
+// CONTENIDOS DESTACADOS
+// Edita este archivo para cambiar las tres cajas de "Últimos contenidos".
+// En la mayoría de casos basta con sustituir url, título, etiqueta, imagen y descripción.
+
+window.MIKETRONIC_CONTENT = [
+  {
+    type: 'article',
+    url: 'AQUI_ENLACE_CAJA_1',
+    tag: 'LLAVES',
+    title: 'Aquí irá un artículo o contenido destacado',
+    description: 'Texto breve de ejemplo para presentar el contenido.',
+    image: ''
+  },
+  {
+    type: 'youtube',
+    url: 'https://www.youtube.com/watch?v=fyD93EkMyh4',
+    tag: 'YOUTUBE',
+    title: 'Vídeo destacado de Miketronic',
+    description: '',
+    channelUrl: 'https://www.youtube.com/@miketronic_'
+  },
+  {
+    type: 'article',
+    url: 'AQUI_ENLACE_ARTICULO_ELECTROMECANIKOS',
+    tag: 'ELECTROMECÁNIKOS',
+    title: 'Artículo destacado de Electromecánikos',
+    description: 'Selecciona el artículo que quieras destacar y sustituye aquí su información.',
+    image: ''
+  }
+];
